99e800373b2cf367124c1498d86eeae66de70d27
Sam <lutherye@gmail.com>
2019-04-11 20:27:34 -0400

    refined moving and jumping

--- src/player.js
+++ src/player.js
@@ -40,36 +40,33 @@ export function draw() {
     ctx.fillStyle = "blue";
     ctx.fill();
     ctx.closePath();
-    char.vel_x = char.vel_x / 2;
     if (keys[65]) {
         if (char.vel_x < speed) {
-            char.vel_x-=2;
+            char.vel_x -= 0.5;
         }
     }
     if (keys[68]) {
-        if (char.vel_x < speed && char.x < canvas.width - char.charWidth) {
-            char.vel_x+=2;
+        if (char.vel_x < speed) {
+            char.vel_x += 0.5;
         }
     }
     if (keys[32] && char.jumped == false) {
             char.vel_y -= 20;
             char.jumped = true;
         }
-    if ( char.y < canvas.height - char.charHeight ) {
-        char.vel_y += 1.5;
-    }
-    if ( char.x < 0 + char.charWidth) {
-        char.x = canvas.width;
-    }
-    if ( char.x > canvas.width - char.charWidth) {
-        
-        char.x = 0;
-    }
 
+    if ( char.x <= 0 ) {
+        char.x = canvas.width - char.charWidth * 2;
+    }
+    if ( char.x >= canvas.width - char.charWidth) {
+        char.x = 0 + char.charWidth;
+    }
+        
+    char.vel_y += 0.7;
     char.y += char.vel_y;
     char.x += char.vel_x;
-    char.x_velocity *= 0.9;
-    char.y_velocity *= 0.9;
+    char.vel_x *= 0.9;
+    char.vel_y *= 0.9;
 
     hitBottom();
 
